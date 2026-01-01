bb8fbccc8c9ca0dd86ce7dbdde1c7efd04e17d02
treinartz <thomas.reinartz@gmail.com>
2026-01-01 08:32:11 -0600

    Add files via upload

--- week11/assignment11/vite.config.js
+++ week11/assignment11/vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: 'react_assignments/week1/exercise1/',
+  base: '/react_assignments3/week11/assignment11/',
 })
